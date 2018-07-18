b83dd1b13873f6c061cd3038ed39381788b077c2
WhiteNalgae <contact@wpws.kr>
2018-07-18 23:39:58 +0900

    ErrorPage Update

---
+++ 403.php
@@ -0,0 +1,45 @@
+<?php
+
+$rand = mt_rand(1,3);
+
+switch($rand){
+    case 1:
+        $msg = "그런 페이지는 접근할 수 없네요";
+        break;
+    case 2:
+        $msg = "방패에 가로막혀서 더이상 나아가지 못해요";
+        break;
+    case 3:
+        $msg = "안타깝게도 당신이랑 방패랑 싸워서 졌어요";
+        break;
+    default:
+        $msg = "에러페이지에서도 에러가 났어요";
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, user-scalable=no">
+         <meta property="og:description" content="WPWS : E403">
+         <meta property="og:title" content="WPWS : E403">
+         <meta property="og:type" content="website">
+         <title>WPWS : 404E</title>
+         <link rel="stylesheet" href="//wpws.kr/error/style.min.css">
+    </head>
+    <body>
+        <div id="center" class="container">
+            <h1>#403</h1>
+            <h2><?php echo $msg; ?></h2>
+            <br />
+            <p>- 해당 경로에 접근 할 권한이 없어요.</p>
+            <p>- 관리자에게 문의하거나 아래 버튼으로 이동해요.</p>
+            <br />
+            <button type="button" onclick="wpws();">WPWS로 돌아가기</button>
+            <button type="button" onclick="othere();">다른 에러페이지 보기</button>
+            <br />
+            <br />
+        </div>
+        <script src="//wpws.kr/error/error.min.js"></script>
+    </body>
+</html>
---
+++ 404.php
@@ -0,0 +1,47 @@
+<?php
+
+$rand = mt_rand(1,3);
+
+
+switch($rand){
+    case 1:
+        $msg = "뭔가 없는데 뭐가 없는지 모르겠어요";
+        break;
+    case 2:
+        $msg = "왠진 몰라도 누가 내 등을 좀 긁어주면 좋겠어요";
+        break;
+    case 3:
+        $msg = "엥? 뭔가 없는데 왜 없는지 저도 몰라요";
+        break;
+    default:
+        $msg = "에러페이지에서도 에러가 났어요";
+        break;
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, user-scalable=no">
+         <meta property="og:description" content="WPWS : E404">
+         <meta property="og:title" content="WPWS : E404">
+         <meta property="og:type" content="website">
+         <title>WPWS : 404E</title>
+         <link rel="stylesheet" href="//wpws.kr/error/style.min.css">
+    </head>
+    <body>
+        <div id="center" class="container">
+            <h1>#404</h1>
+            <h2><?php echo $msg; ?></h2>
+            <br />
+            <p>- 서버에서 해당 경로를 찾을 수 없어요.</p>
+            <p>- 관리자에게 문의하거나 아래 버튼으로 이동해요.</p>
+            <br />
+            <button type="button" onclick="wpws();">WPWS로 돌아가기</button>
+            <button type="button" onclick="othere();">다른 에러페이지 보기</button>
+            <br />
+            <br />
+        </div>
+        <script src="//wpws.kr/error/error.min.js"></script>
+    </body>
+</html>
